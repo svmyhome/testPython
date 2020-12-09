b36f025883406b10905aa07e47672d5324da7175
Vladimir Sarychev <svmyhome@mail.ru>
2020-12-09 23:51:03 +0300

    Many files added

--- import_mod.py
+++ import_mod.py
@@ -1,5 +1,14 @@
-import math
+# import math
+#
+# fl = float(input())
+#
+# print((2 * math.pi) * fl)
 
-fl = float(input())
-
-print((2 * math.pi) * fl)
+import sys
+import subprocess
+x = ''
+print(*sys.argv[1:])
+# for i in sys.argv[1:]:
+#     x += i
+# #print(len(sys.argv))
+# print(x)
