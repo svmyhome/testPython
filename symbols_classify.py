16000f49a545bb4eb350a903841c87a574c8761a
Vladimir Sarychev <svmyhome@mail.ru>
2021-11-21 19:18:22 +0300

    Добавлен файл c классификацией символов в сьроке

---
+++ symbols_classify.py
@@ -0,0 +1,49 @@
+#стоит ли исходная строка из буквенно-цифровых символов.
+# s1 = 'abc123'
+# s2 = 'abc$*123'
+# s3 = ''
+
+# print(s1.isalnum())
+# print(s2.isalnum())
+# print(s2.isalnum())
+
+#состоит ли исходная строка из буквенных символов
+# s1 = 'Aabc'
+# s2 = 'abc123'
+# s3 = ''
+# print(s1.isalpha())
+# print(s2.isalpha())
+# print(s2.isalpha())
+
+# # состоит ли исходная строка только из цифровых символов
+# s1 = '1234567'
+# s2 = 'abc123'
+# s3 = ''
+#
+# print(s1.isdigit())
+# print(s2.isdigit())
+# print(s3.isdigit())
+
+#являются ли все буквенные символы исходной строки строчными
+# s1 = 'abc'
+# s2 = 'abc1$d'
+# s3 = 'Abc1$D'
+# print(s1.islower())
+# print(s2.islower())
+# print(s3.islower())
+
+#являются ли все буквенные символы исходной строки заглавными
+
+s1 = 'ABC'
+s2 = 'ABC1$D'
+s3 = 'Abc1$D'
+
+print(s1.isupper())
+print(s2.isupper())
+print(s3.isupper())
+
+#состоит ли исходная строка только из пробельных символов
+# s1 = '       '
+# s2 = 'abc1$d'
+# print(s1.isspace())
+# print(s2.isspace())
